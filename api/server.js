8b60722807da2cc4ee6983e35a43be6fb226495e
maks <11082252+maks112v@users.noreply.github.com>
2022-08-10 00:08:33 -0400

    added get requests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
-// BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.get('/api/users', async (_, res) => {
+  const users = await User.find();
+
+  return res.status(200).json(users);
+});
+
+server.get('/api/users/:id', async (req, res) => {
+  const user = await User.findById(req?.params?.id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'The user with the specified ID does not exist',
+    });
+  }
+
+  return res.status(200).json(user);
+});
+
+module.exports = server;
